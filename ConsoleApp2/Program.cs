2173f45f0641e92661800bdbeaaa4017da95b574
Joona Pyykkönen <s2426501@edu.savonia.fi>
2025-10-07 15:19:11 +0300

    tästä tämmönen muutos

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -5,6 +5,13 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            // sipulipipuli ja valkosipulipipuli
+            Console.WriteLine("Pösilö lol");
+            Console.WriteLine("Piti tehtdä muutos");
+
+
+
         }
     }
 }
